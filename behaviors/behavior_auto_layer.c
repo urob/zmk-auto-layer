b8a8f04dcd7c979be30fa601f0becfb1e38b3088
urob <978080+urob@users.noreply.github.com>
2024-08-01 20:14:47 -0400

    Make layer a parameter

--- behaviors/behavior_auto_layer.c
+++ behaviors/behavior_auto_layer.c
@@ -33,9 +33,11 @@ struct zmk_key_param {
         .modifiers = SELECT_MODS(param), .page = ZMK_HID_USAGE_PAGE(param),                        \
         .id = ZMK_HID_USAGE_ID(param),                                                             \
     }
-#endif // end backport
+#endif // ZMK_KEY_PARAM_DECODE
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#define ZMK_BHV_AUTO_LAYER_MAX_ACTIVE 10
 
 struct key_list {
     size_t size;
@@ -44,47 +46,78 @@ struct key_list {
 
 struct behavior_auto_layer_config {
     const struct key_list *continue_keys;
-    uint8_t layers;
     bool ignore_alphas;
     bool ignore_numbers;
     bool ignore_modifiers;
 };
 
-struct behavior_auto_layer_data {
-    bool active;
+struct active_auto_layer {
+    bool is_active;
+    uint8_t layer;
+    const struct behavior_auto_layer_config *config;
 };
 
-static void activate_auto_layer(const struct device *dev) {
-    struct behavior_auto_layer_data *data = dev->data;
-    const struct behavior_auto_layer_config *config = dev->config;
+struct active_auto_layer active_auto_layers[ZMK_BHV_AUTO_LAYER_MAX_ACTIVE] = {};
 
-// We check whether the track-momentary-layers patch is enabled, because
-// zmk_keymap_layer_activate takes two arguments if it is.
-#if IS_ENABLED(CONFIG_ZMK_TRACK_MOMENTARY_LAYERS)
-    zmk_keymap_layer_activate(config->layers, false);
-#else
-    zmk_keymap_layer_activate(config->layers);
-#endif
-    data->active = true;
+static struct active_auto_layer *find_auto_layers(uint8_t layer) {
+    for (int i = 0; i < ZMK_BHV_AUTO_LAYER_MAX_ACTIVE; i++) {
+        if (active_auto_layers[i].layer == layer && active_auto_layers[i].is_active) {
+            return &active_auto_layers[i];
+        }
+    }
+    return NULL;
 }
 
-static void deactivate_auto_layer(const struct device *dev) {
-    struct behavior_auto_layer_data *data = dev->data;
-    const struct behavior_auto_layer_config *config = dev->config;
+static int new_auto_layer(uint8_t layer, const struct behavior_auto_layer_config *config,
+                          struct active_auto_layer **auto_layer) {
+    for (int i = 0; i < ZMK_BHV_AUTO_LAYER_MAX_ACTIVE; i++) {
+        struct active_auto_layer *const ref_auto_layer = &active_auto_layers[i];
+        if (!ref_auto_layer->is_active) {
+            ref_auto_layer->is_active = true;
+            ref_auto_layer->layer = layer;
+            ref_auto_layer->config = config;
+            *auto_layer = ref_auto_layer;
+            return 0;
+        }
+    }
+    return -ENOMEM;
+}
 
-    zmk_keymap_layer_deactivate(config->layers);
-    data->active = false;
+static void clear_auto_layer(struct active_auto_layer *auto_layer) {
+    auto_layer->is_active = false;
+}
+
+static void activate_auto_layer(struct active_auto_layer *auto_layer) {
+#if IS_ENABLED(CONFIG_ZMK_TRACK_MOMENTARY_LAYERS)
+    // If patch is enabled, second argument signals whether layer change momentary or not
+    zmk_keymap_layer_activate(auto_layer->layer, false);
+#else
+    zmk_keymap_layer_activate(auto_layer->layer);
+#endif // IS_ENABLED(CONFIG_ZMK_TRACK_MOMENTARY_LAYERS)
+}
+
+static void deactivate_auto_layer(struct active_auto_layer *auto_layer) {
+    zmk_keymap_layer_deactivate(auto_layer->layer);
+    auto_layer->is_active = false;
 }
 
 static int on_auto_layer_binding_pressed(struct zmk_behavior_binding *binding,
                                          struct zmk_behavior_binding_event event) {
     const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
-    struct behavior_auto_layer_data *data = dev->data;
+    const struct behavior_auto_layer_config *cfg = dev->config;
 
-    if (data->active) {
-        deactivate_auto_layer(dev);
+    struct active_auto_layer *auto_layer;
+    auto_layer = find_auto_layers(binding->param1);
+    if (auto_layer == NULL) {
+        if (new_auto_layer(binding->param1, cfg, &auto_layer) == -ENOMEM) {
+            LOG_ERR("Unable to create new auto_layer. Insufficient space in active_auto_layers[].");
+            return ZMK_BEHAVIOR_OPAQUE;
+        }
+        activate_auto_layer(auto_layer);
+        LOG_DBG("%d created new auto_layer", event.position);
     } else {
-        activate_auto_layer(dev);
+        deactivate_auto_layer(auto_layer);
+        LOG_DBG("%d deactivated auto_layer", event.position);
     }
 
     return ZMK_BEHAVIOR_OPAQUE;
@@ -104,11 +137,6 @@ static int auto_layer_keycode_state_changed_listener(const zmk_event_t *eh);
 
 ZMK_LISTENER(behavior_auto_layer, auto_layer_keycode_state_changed_listener);
 ZMK_SUBSCRIPTION(behavior_auto_layer, zmk_keycode_state_changed);
-
-#define DEVICE_COUNT DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT)
-#define DEVICE_INST(n, _) DEVICE_DT_GET(DT_DRV_INST(n))
-
-static const struct device *devs[] = {LISTIFY(DEVICE_COUNT, DEVICE_INST, (, ))};
 
 static bool key_list_contains(const struct key_list *list, uint16_t usage_page, zmk_key_t usage_id,
                               zmk_mod_flags_t modifiers) {
@@ -172,25 +200,31 @@ static int auto_layer_keycode_state_changed_listener(const zmk_event_t *eh) {
         return ZMK_EV_EVENT_BUBBLE;
     }
 
-    for (int i = 0; i < ARRAY_SIZE(devs); i++) {
-        const struct device *dev = devs[i];
-        const struct behavior_auto_layer_data *data = dev->data;
-        if (!data->active) {
+    for (int i = 0; i < ZMK_BHV_AUTO_LAYER_MAX_ACTIVE; i++) {
+        struct active_auto_layer *auto_layer = &active_auto_layers[i];
+        if (!auto_layer->is_active) {
             continue;
         }
 
-        const struct behavior_auto_layer_config *config = dev->config;
-
-        if (!auto_layer_should_continue(config, ev)) {
+        if (!auto_layer_should_continue(auto_layer->config, ev)) {
             LOG_DBG("Deactivating auto_layer for 0x%02X - 0x%02X", ev->usage_page, ev->keycode);
-            deactivate_auto_layer(dev);
+            deactivate_auto_layer(auto_layer);
         }
     }
 
     return ZMK_EV_EVENT_BUBBLE;
 }
 
-static int behavior_auto_layer_init(const struct device *dev) { return 0; }
+static int behavior_auto_layer_init(const struct device *dev) {
+    static bool init_first_run = true;
+    if (init_first_run) {
+        for (int i = 0; i < ZMK_BHV_AUTO_LAYER_MAX_ACTIVE; i++) {
+            clear_auto_layer(&active_auto_layers[i]);
+        }
+    }
+    init_first_run = false;
+    return 0;
+}
 
 #define KEY_LIST_ITEM(i, n, prop) ZMK_KEY_PARAM_DECODE(DT_INST_PROP_BY_IDX(n, prop, i))
 
@@ -204,16 +238,13 @@ static int behavior_auto_layer_init(const struct device *dev) { return 0; }
 
 #define KP_INST(n)                                                                                 \
     static const struct key_list auto_layer_continue_list_##n = PROP_KEY_LIST(n, continue_list);   \
-                                                                                                   \
-    static struct behavior_auto_layer_data behavior_auto_layer_data_##n = {.active = false};       \
     static struct behavior_auto_layer_config behavior_auto_layer_config_##n = {                    \
-        .layers = DT_INST_PROP(n, layers),                                                         \
         .continue_keys = &auto_layer_continue_list_##n,                                            \
         .ignore_alphas = DT_INST_PROP(n, ignore_alphas),                                           \
         .ignore_numbers = DT_INST_PROP(n, ignore_numbers),                                         \
         .ignore_modifiers = DT_INST_PROP(n, ignore_modifiers),                                     \
     };                                                                                             \
-    BEHAVIOR_DT_INST_DEFINE(n, behavior_auto_layer_init, NULL, &behavior_auto_layer_data_##n,      \
+    BEHAVIOR_DT_INST_DEFINE(n, behavior_auto_layer_init, NULL, NULL,                               \
                             &behavior_auto_layer_config_##n, POST_KERNEL,                          \
                             CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_auto_layer_driver_api);
 
