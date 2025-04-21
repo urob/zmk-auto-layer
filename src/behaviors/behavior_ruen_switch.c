7745832da6b4d66e3edb5a99bc566d39ade54460
jewtvet <koala.lite.yt@gmail.com>
2025-04-21 05:45:30 -0400

    4

--- src/behaviors/behavior_ruen_switch.c
+++ src/behaviors/behavior_ruen_switch.c
@@ -10,6 +10,11 @@ static int on_ruen_switch_pressed(struct zmk_behavior_binding *binding, struct z
 }
 
 static int on_ruen_switch_released(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
+    uint32_t code = binding->param2;
+    raise_zmk_keycode_state_changed_from_encoded(code, true, event.timestamp);
+    k_msleep(10);
+    raise_zmk_keycode_state_changed_from_encoded(code, false, event.timestamp + 10);
+    k_msleep(50);
     bool is_eng = binding->param1 != 0;
     zmk_lang_set_state(is_eng);
     return ZMK_BEHAVIOR_OPAQUE;
