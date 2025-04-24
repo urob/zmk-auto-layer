7d93f53c86e8c2499a82573703f32ad120ace741
jewtvet <koala.lite.yt@gmail.com>
2025-04-24 17:05:59 -0400

    is_mac 2

--- src/lang.c
+++ src/lang.c
@@ -1,3 +1,5 @@
+#include <zephyr/init.h>
+#include <zephyr/device.h>
 #include <zephyr/settings/settings.h>
 #include <zmk/lang.h>
 #include <stdbool.h>
@@ -23,23 +25,23 @@ void zmk_lang_set_mac(bool state) {
     settings_save_one("ruen/is_mac", &is_mac, sizeof(is_mac));
 }
 
-static int lang_settings_set(const char *key, size_t len, settings_read_cb read_cb, void *cb_arg) {
+static int ruen_settings_set(const char *key, size_t len, settings_read_cb read_cb, void *cb_arg) {
     if (strcmp(key, "is_mac") == 0 && len == sizeof(is_mac)) {
         read_cb(cb_arg, &is_mac, len);
     }
     return 0;
 }
 
-static struct settings_handler lang_settings_handler = {
+static struct settings_handler ruen_settings_handler = {
     .name = "ruen",
-    .h_set = lang_settings_set,
+    .h_set = ruen_settings_set,
 };
 
-static int lang_init(const struct device *dev) {
+static int ruen_init(const struct device *dev) {
+    ARG_UNUSED(dev);
     settings_subsys_init();
-    settings_register(&lang_settings_handler);
+    settings_register(&ruen_settings_handler);
     settings_load_subtree("ruen");
     return 0;
 }
-
-SYS_INIT(lang_init, APPLICATION, CONFIG_SETTINGS_INIT_PRIORITY);
+SYS_INIT(ruen_init, APPLICATION, CONFIG_SETTINGS_INIT_PRIORITY);
